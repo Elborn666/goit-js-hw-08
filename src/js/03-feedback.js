00be4bfa27234d8baa3a23735f341fd8074f5e52
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-03-30 00:30:51 +0300

    correct task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,34 @@
 import throttle from 'lodash.throttle';
 
-const Refs = {
-    form: document.querySelector(".feedback-form"),
-    input: document.querySelector(".feedback-form input"),
-    textarea: document.querySelector(".feedback-form textarea"),
+const form = document.querySelector(".feedback-form");
+
+let dataForm = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+const { email, message } = form.elements;
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputData, 500));
+
+
+function onInputData(event){
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
 }
-
-const formData = {}
-
-Refs.form.addEventListener('submit', onFormSubmit);
-Refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-Refs.form.addEventListener('input', (element) => {
-    console.log(element.target.name)
-    console.log(element.target.value)
-
-    formData[element.target.name] = element.target.value;
-
-    console.log(formData)
-})
 
 function onFormSubmit(event){
     event.preventDefault();
     
     console.log('Submit form')
 
+    localStorage.removeItem('feedback-form-state');
     event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state')
-}
-
-function onTextareaInput(event){
-    const valueMsg = event.target.value
-
-    localStorage.setItem('feedback-form-state', valueMsg)
+    dataForm = {};
 }
 
 function onMsgOutput(){
-    const saveMsg = localStorage.getItem('feedback-form-state')
-    console.log(saveMsg)
-
-    if(saveMsg){
-        Refs.textarea.value = saveMsg;   
-    }
+    if (dataForm) {
+        email.value = dataForm.email || '';
+        message.value = dataForm.message || '';
+      }
 }
 
 onMsgOutput()
